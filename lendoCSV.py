10da75623de435306a62b1402ef118f93d0b6a9f
marcelovieiratecnologia <marcelovieiratecnologia@gmail.com>
2020-02-13 12:41:13 -0300

    alterado o requirements.txt pelo manjaro

--- lendoCSV.py
+++ lendoCSV.py
@@ -1,11 +1,23 @@
 
-import pandas as pd
-print(pd.read_csv('clinica.csv', encoding='latin-1'))
+# import pandas as pd
+# print(pd.read_csv('clinica.csv', encoding='latin-1'))
 
 # import csv
-#
-# with open('../ArquivosClinica/2019 AGOSTO - Planilha Caixa OdontoMed.csv', 'rb') as arq:
-#     linhas = csv.reader(arq, delimiter=':', quoting=csv.QUOTE_NONE)
-#     for linha in linhas:
+# with open('clinica.csv', 'rb') as arq:
+#     for linha in arq.readline():
 #         print(linha)
+#     # print(arq.readline())
 #
+#     # linhas = csv.reader(arq, delimiter=':', quoting=csv.QUOTE_NONE)
+#     # for linha in linhas:
+#     #     print(linha)
+
+
+arquivo = open('clinica.csv', 'r', encoding='latin-1')
+linhas = arquivo.readlines()
+countLinha = 0
+for linha in linhas:
+    countLinha = countLinha + 1
+    print(linha)
+
+print('Total de linhas é: ', countLinha)
