a0413aa603ff6a42308f69b67f922130d46df4de
marcelovieiratecnologia <marcelovieiratecnologia@gmail.com>
2020-02-28 10:34:31 -0300

    alterações paradas

--- lendoCSV.py
+++ lendoCSV.py
@@ -1,23 +1,32 @@
+import csv
 
-# import pandas as pd
-# print(pd.read_csv('clinica.csv', encoding='latin-1'))
-
-# import csv
-# with open('clinica.csv', 'rb') as arq:
-#     for linha in arq.readline():
-#         print(linha)
-#     # print(arq.readline())
 #
-#     # linhas = csv.reader(arq, delimiter=':', quoting=csv.QUOTE_NONE)
-#     # for linha in linhas:
-#     #     print(linha)
+# usando pandas, porém tenho que instalar a biblioteca
+# import pandas as pd
+# print(pd.read_csv('clinica.csv', 'rb', encoding='latin-1'))
 
+# arquivo = open('clinica.csv', 'r', encoding='latin-1')
+# def lerArq(arquivo):
+#     linhas = arquivo.readlines()
+#     countLinha = 0
+#     for linha in linhas:
+#         countLinha = countLinha + 1
+#         print(linha)
+#     # print('Total de linhas é: ', countLinha)
+#     return linhas
+# lerArq(arquivo)
 
-arquivo = open('clinica.csv', 'r', encoding='latin-1')
-linhas = arquivo.readlines()
-countLinha = 0
-for linha in linhas:
-    countLinha = countLinha + 1
-    print(linha)
+# usando o csv
+row = []
+# arquivo = 'clinica.csv'
+def lerArq(arquivo):
+    with open(arquivo, encoding='latin-1') as csvfile:
+        readCSV = csv.reader(csvfile,delimiter=';')
+        # print(readCSV)
+        for rows in readCSV:
+            # print(row)
+            row.append(rows)
+    return row
 
-print('Total de linhas é: ', countLinha)
+# l = lerArq(arquivo)
+# print(l[0][0])
--- main.py
+++ main.py
@@ -2,6 +2,28 @@
 import psycopg2
 import csv
 from config import config
+from lendoCSV import lerArq
+
+# arquivo = 'clinica.csv'
+# linhas = lerArq(arquivo)
+# print(linhas[:][:]) # Todas as Linhas e Colunas do arquivo
+
+# Variáveis Globais
+linhas = []
+
+
+''' Layout do Arquivo:
+ ===== ============== ========== ===================== ========== ============ ============ === ========= ======= ======= ========== ===============  
+|  1  |       2      |    3     |          4          |   5      |      6     |      7     | 8 |    9    |   10  |  11   |    12    |      13       |
+ ===== ============== ========== ===================== ========== ============ ============ === ========= ======= ======= ========== ===============
+|DATA | PROFISSIONAL | PACIENTE | CONVENIO/PARTICULAR |  D/C     | TP CRÉDITO | N PARCELAS | % | ENTRADA | SAIDA | SALDO | DESCONTO | TOTAL LIQUIDO |
+ ===== ============== ========== ===================== ========== ============ ============ === ========= ======= ================== =============== 
+                                    particular          débito      a vista
+                                    odontoprev          crédito     a prazo
+                                    bradesco
+                                    interodonto
+====================================================================================================================================================                                    
+'''
 
 def connect():
     """ Connect to the PostgreSQL database server """
@@ -9,48 +31,79 @@ def connect():
     try:
         # Ler os parametros de conexão
         params = config()
-
         # conecta no PostGreSQL Server
-        print('Connecting to the PostgreSQL database...')
+        # print('Connecting to the PostgreSQL database...')
         conn = psycopg2.connect(**params)
-
         # Cria um cursor
-        cur = conn.cursor()
-
-        # Executa uma Declaração
-        print('PostgreSQL database version:')
-        cur.execute('SELECT version()')
-
-        # display the PostgreSQL database server version
-        db_version = cur.fetchone()
-        print(db_version)
-
-        # close the communication with the PostgreSQL
-        cur.close()
+        # cur = conn.cursor() Irei criar o cursor apenas na minha função
+        # @@@@@@ Não estou usando essas instruções abaixo @@@@@@@@
+        # # Executa uma Declaração
+        # print('PostgreSQL database version:')
+        # cur.execute('SELECT version()')
+        # # display the PostgreSQL database server version
+        # db_version = cur.fetchone()
+        # print(db_version)
+        # # close the communication with the PostgreSQL
+        # cur.close()
+        # @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
     except (Exception, psycopg2.DatabaseError) as error:
         print(error)
-    finally:
-        if conn is not None:
-            conn.close()
-            print('Conexão Finalizada/Fechada.')
+    # finally:
+    #     if conn is not None:
+    #         conn.close()
+    #         print('Conexão Finalizada/Fechada.')
+    return conn
+
+def testeConectaPostGres_ExecutaSql():
+    # print(connect())
+    conectar = connect()
+    cur = conectar.cursor()
+    sql = "Select * from tb_om_entrada_saida;"
+    cur.execute(sql)
+    resultado = cur.fetchall()
+    print(resultado)
+
+def insertPostgres_tb_om_entrada_saida():
+    conectar = connect()
+    cursor = conectar.cursor()
+    # sqlInsert = 'Insert into tb_om_cidades (id, nome_cidade, uf) values (%s,%s,%s);'
+    # value = (99, 'CIDADE', 'GG')
+    sqlInsert = "INSERT INTO tb_om_entrada_saida () values()"
+    cursor.execute(sqlInsert, value)
+    conectar.commit()
 
 
-def sql():
-    #conn = None
-    # Ler os parametros de conexão
-    params = config()
-    # conecta no PostGreSQL Server
-    conn = psycopg2.connect(**params)
-    # Cria um cursor
-    cursor = conn.cursor()
-    # Uma declaração
-    SqlStm = 'SELECT * FROM tb_om_cidades;'
-    cursor.execute(SqlStm)
-    # bases = cursor.fetchone()
-    bases = cursor.fetchall()
+# def sql():
+#     #conn = None
+#     # Ler os parametros de conexão
+#     params = config()
+#     # conecta no PostGreSQL Server
+#     conn = psycopg2.connect(**params)
+#     # Cria um cursor
+#     cursor = conn.cursor()
+#     # Uma declaração
+#     SqlStm = 'SELECT * FROM tb_om_entrada_saida;'
+#     cursor.execute(SqlStm)
+#     # rows = cursor.fetchone()
+#     rows = cursor.fetchall()
+#     # print(rows[:])
+#     for row in rows:
+#         print(row[:])
 
-    print(bases[:])
+
 
 if __name__ == '__main__':
-    sql()
+    # testeConectaPostGres_ExecutaSql()
+
+    arquivo = 'clinica.csv'
+    # linhas var global
+    linhas = lerArq(arquivo)
+    # print(linhas[0][0])
+    # print(linhas[0])
+    # sql()
     # connect()
+    #insert()
+    insertPostgres_tb_om_entrada_saida()
+
+
+
